848557fc40a6dbd034aca6c7afb7605d135057bc
Tatyana <tatyana8787@gmail.com>
2019-04-30 20:40:00 +0200

    Фильтрация данных

--- templates/index.php
+++ templates/index.php
@@ -4,7 +4,7 @@
     <ul class="promo__list">
         <?php foreach ($categories as $category): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="../pages/all-lots.html"><?= esc($category); ?></a>
+                <a class="promo__link" href="../pages/all-lots.html"><?= $category; ?></a>
             </li>
         <?php endforeach; ?>
     </ul>
