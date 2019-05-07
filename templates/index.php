5c221b2b6b813fa3870dbed99ec7bd80e160f7b3
Tatyana <tatyana8787@gmail.com>
2019-05-07 21:27:48 +0200

    Работа с MySQL из PHP-сценариев

--- templates/index.php
+++ templates/index.php
@@ -3,8 +3,8 @@
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
         <?php foreach ($categories as $category): ?>
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="../pages/all-lots.html"><?= $category; ?></a>
+            <li class="promo__item promo__item--<?= $category['symbol_code']; ?>">
+                <a class="promo__link" href="../pages/all-lots.html"><?= $category['name']; ?></a>
             </li>
         <?php endforeach; ?>
     </ul>
@@ -17,15 +17,15 @@
         <?php foreach ($lots as $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $lot['img']; ?>" width="350" height="260" alt="">
+                    <img src="<?= $lot['img_path']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= esc($lot['category']); ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="../pages/lot.html"><?= esc($lot['name']); ?></a></h3>
+                    <span class="lot__category"><?= esc($lot['name']); ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="../pages/lot.html"><?= esc($lot['title']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= price_format(esc($lot['price'])); ?></span>
+                            <span class="lot__cost"><?= price_format(esc($lot['sum_start'])); ?></span>
                         </div>
                         <?= timer("tomorrow"); ?>
                     </div>
